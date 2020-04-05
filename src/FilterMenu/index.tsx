c0243e4e85d8b74257228864847247500d7ea27b
Vincent Ahrend <mail@vincentahrend.com>
2020-04-05 21:24:52 +0200

    Clean up

--- src/FilterMenu/index.tsx
+++ src/FilterMenu/index.tsx
@@ -2,7 +2,6 @@ import React from "react";
 import { makeStyles, Theme, createStyles } from "@material-ui/core/styles";
 import Drawer from "@material-ui/core/Drawer";
 import List from "@material-ui/core/List";
-import Divider from "@material-ui/core/Divider";
 import ListItem from "@material-ui/core/ListItem";
 import ListItemIcon from "@material-ui/core/ListItemIcon";
 import ListItemText from "@material-ui/core/ListItemText";
