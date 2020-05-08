a31e8d000533006e881c5a2cf26dc2f5eb9981d5
Vincent Ahrend <mail@vincentahrend.com>
2020-05-08 13:20:19 +0200

    Add transformer for group data

---
+++ scripts/transform_groups.py
@@ -0,0 +1,62 @@
+import csv
+import json
+
+IN_PATH = "data/groups.csv"
+OUT_PATH = "public/data/groups.geojson"
+
+
+def valid(group):
+    if group["Koordinaten"] == "":
+        return False
+
+    return True
+
+
+def read_data():
+    with open(IN_PATH) as f:
+        rv = [
+            group
+            for group in csv.DictReader(f, delimiter=";", dialect="excel")
+            if valid(group)
+        ]
+    return rv
+
+
+def transform_geojson(data):
+    rv = {"features": [], "type": "FeatureCollection"}
+
+    def decode_coordinates(row):
+        x, y = row["Koordinaten"].split(", ")
+        return list(map(float, [x, y]))
+
+    for row in data:
+        feature = {
+            "type": "Feature",
+            "properties": {
+                "title": row["Name Organisation/Gruppen"],
+                "city": row["Ort (Stadt)"],
+                "website": row["Webseite"],
+                "contact": row["Kontakt (email)"],
+                "plants": row["Kraftwerke"],
+                "note": row["Besonderheit"],
+                "news": row["News"],
+            },
+            "geometry": {"coordinates": decode_coordinates(row), "type": "Point"},
+        }
+        rv["features"].append(feature)
+    return rv
+
+
+def write_out(geojson):
+    with open(OUT_PATH, "w+") as f:
+        json.dump(geojson, f, ensure_ascii=False, indent=2)
+
+
+def main():
+    data = read_data()
+    geojson = transform_geojson(data)
+    write_out(geojson)
+
+
+if __name__ == "__main__":
+    main()
